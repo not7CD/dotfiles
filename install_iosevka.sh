bd0dc28b5e98f4cb4cb7b278a7200307a3b4ec0d
Norbert Szulc <norbert@not7cd.net>
2019-05-19 15:31:44 +0200

    Add convinience script for Iosevka font

---
+++ install_iosevka.sh
@@ -0,0 +1,14 @@
+if [ ! $(fc-list | grep -q "Iosevka") ]; then
+	echo "Iosevka not found"
+	FONT_URL=$(curl -s https://api.github.com/repos/be5invis/Iosevka/releases/latest | grep browser_download_url | head -n 1 | cut -d '"' -f 4)
+	echo "Downloading latest Iosevka font $FONT_URL"
+	if [ ! -d /usr/local/share/fonts/iosevka ]; then
+		mkdir /usr/local/share/fonts/iosevka
+	fi
+	curl -SL -o /tmp/iosevka.zip $FONT_URL 
+	unzip /tmp/iosevka.zip -d /tmp/iosevka
+	cp -R /tmp/iosevka/ttf/*.ttf /usr/local/share/fonts/iosevka 
+
+	fc-cache -f -v
+fi
+fc-list | grep -q "Iosevka"
